4f61ab0878d71f5f40c8dce0015c624f21dd317d
Sávio <saviopf22@gmail.com>
2022-05-09 23:42:57 -0300

    mostrando posts

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <Link href="/">My Dev Blog</Link>
+      </div>
+    </header>
+  );
+}
